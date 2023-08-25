a9988cc5086941221f4471eb864ba2ad41156cef
Joao Astolfi <jpastolfi@gmail.com>
2023-08-25 21:48:43 +0100

    Requisito 02

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    displayName: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+  return User;
+}
