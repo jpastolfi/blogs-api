87a46c22b16f74f7630de0bd0e742374cb985e57
Joao Astolfi <jpastolfi@gmail.com>
2023-08-28 19:07:10 +0100

    Correção req 05

--- src/controllers/Post.controller.js
+++ src/controllers/Post.controller.js
@@ -3,6 +3,7 @@ const postService = require('../services/Post.service');
 const insert = async (req, res) => {
   const { title, content, categoryIds } = req.body;
   const newPost = postService.insert(title, content, categoryIds);
+  console.log(newPost);
   return res.status(201).json({ title, content, categoryIds });
 };
 
